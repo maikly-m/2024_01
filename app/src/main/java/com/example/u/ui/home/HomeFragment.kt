f6ebbf757e61b7c929031030415e8e3d1d0c4b7f
emoji <877482941@qq.com>
2024-11-29 16:33:18 +0800

    init 5

--- app/src/main/java/com/example/u/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/u/ui/home/HomeFragment.kt
@@ -3,11 +3,24 @@ package com.example.u.ui.home
 import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
+import android.text.InputType
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.BackgroundColorSpan
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -35,14 +48,88 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val editText: EditText = binding.textHome
+        // 替换闪烁的cursor
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // For Android 10 and above (API 29+)
+            val cursorDrawable = resources.getDrawable(R.drawable.custom_cursor, null)
+            editText.textCursorDrawable = cursorDrawable
+        } else {
+            // For Android 9 and below
+            val editorField = EditText::class.java.getDeclaredField("mEditor")
+            editorField.isAccessible = true
+            val editor = editorField.get(editText)
+
+            val cursorDrawable = resources.getDrawable(R.drawable.custom_cursor)
+            val cursorField = editor.javaClass.getDeclaredField("mCursorDrawable")
+            cursorField.isAccessible = true
+            cursorField.set(editor, arrayOf(cursorDrawable, cursorDrawable))
         }
+
+        //editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+        homeViewModel.text.observe(viewLifecycleOwner) {
+
+        }
+
+        // spannableString
+        val tvTip: TextView = binding.tvTip
+        val spannableString = SpannableString("123abc345fgh6666ffflipokkkkk")
+        spannableString.setSpan(ForegroundColorSpan(Color.GRAY), 3,
+            6, Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+        spannableString.setSpan(ForegroundColorSpan(Color.BLUE), 8,
+            11, Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+//        // 为“bold”部分应用粗体样式
+//        spannableString.setSpan(
+//            StyleSpan(Typeface.BOLD),  // 设置粗体样式
+//            10, 14,  // 开始和结束索引
+//            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+//        )
+//        // 为“italic”部分应用斜体样式
+//        spannableString.setSpan(
+//            StyleSpan(Typeface.ITALIC),  // 设置斜体样式
+//            15, 21,  // 开始和结束索引
+//            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+//        )
+//        // 为“underline”部分添加下划线
+//        spannableString.setSpan(
+//            UnderlineSpan(), // 添加下划线
+//            10, 18, // 开始和结束索引
+//            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+//        )
+        // 为“Click here”部分添加点击事件
+        spannableString.setSpan(object : ClickableSpan() {
+            override fun updateDrawState(ds: TextPaint) {
+                // super.updateDrawState(ds)
+            }
+
+            override fun onClick(widget: View) {
+                // 在点击后修改背景颜色
+                // 改变背景颜色
+                spannableString.setSpan(
+                    BackgroundColorSpan(Color.TRANSPARENT), // 改为原来透明颜色
+                    3, 6, // "Click here" 部分
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                // 更新 TextView 内容以刷新背景颜色
+                widget.invalidate()
+                // 处理点击事件
+                Toast.makeText(requireContext(), "Clicked on Click here", Toast.LENGTH_SHORT).show()
+            }
+        }, 3, 6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tvTip.setText(spannableString)
+        tvTip.movementMethod = LinkMovementMethod.getInstance()
 
         binding.btnPermission.setOnClickListener {
             PermissionX.init(requireActivity())
-                .permissions(Manifest.permission.READ_CONTACTS, Manifest.permission.CAMERA, Manifest.permission.CALL_PHONE)
+                .permissions(
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.CALL_PHONE
+                )
                 .setDialogTintColor(Color.parseColor("#1972e8"), Color.parseColor("#8ab6f5"))
                 .onExplainRequestReason { scope, deniedList, beforeRequest ->
                     val message = "PermissionX needs following permissions to continue"
@@ -57,9 +144,14 @@ class HomeFragment : Fragment() {
                 }
                 .request { allGranted, grantedList, deniedList ->
                     if (allGranted) {
-                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT)
+                            .show()
                     } else {
-                        Toast.makeText(activity, "The following permissions are denied：$deniedList", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            activity,
+                            "The following permissions are denied：$deniedList",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
         }
@@ -94,7 +186,7 @@ class HomeFragment : Fragment() {
                 permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             }
         }
-        if (permissions.size > 0){
+        if (permissions.size > 0) {
             PermissionX.init(requireActivity())
                 .permissions(permissions)
                 .setDialogTintColor(Color.parseColor("#1972e8"), Color.parseColor("#8ab6f5"))
@@ -108,9 +200,14 @@ class HomeFragment : Fragment() {
                 }
                 .request { allGranted, grantedList, deniedList ->
                     if (allGranted) {
-                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT)
+                            .show()
                     } else {
-                        Toast.makeText(activity, "The following permissions are denied：$deniedList", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            activity,
+                            "The following permissions are denied：$deniedList",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
         }
@@ -120,4 +217,15 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    fun togglePasswordEditTextVisibility(editText: EditText) {
+        val textPassword = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        val textVisiblePassword = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+        editText.inputType = if (editText.inputType == textPassword) {
+            textVisiblePassword
+        } else {
+            textPassword
+        }
+    }
+
 }
